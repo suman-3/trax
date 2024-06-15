748df777d1b75177456affb609166c970ebfc4de
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-15 11:51:59 +0530

    small chanage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,7 +8,7 @@ const MarketingPage = () => {
       <div className="flex items-center justify-center flex-col">
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
-          <h1 className="text-lg font-semibold"> No 1 Task management</h1>
+          <h1 className="text-lg font-bold"> No 1 Task management</h1>
         </div>
         <h1 className="text-3xl md:text-6xl text-clip text-neutral-800 mb-6">
           TraX helps team move
