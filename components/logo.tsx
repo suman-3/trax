a9d355190027f94ed0ea5ae968dcac4117d5dbed
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-15 12:28:09 +0530

    logo added

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="items-center hover:opacity-75 transition gap-x-2 hidden md:flex">
+      <div className="items-center hover:opacity-75 transition gap-x-1 hidden md:flex">
+        <Image src='/logo.svg' alt="logo" width={40} height={40}  />
         <h1 className="text-4xl font-bold">
           Tra
           <span className="text-fuchsia-700">X</span>
