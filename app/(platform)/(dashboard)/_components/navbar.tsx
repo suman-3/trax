b58379c2b5cd32723c66bb36ac518fa348416245
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-16 10:56:35 +0530

    mobile navbar added

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,11 +2,12 @@ import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
   return (
     <nav className="fixed z-20 top-0 w-full h-16 border-b shadow-sm bg-white flex items-center px-5">
-      {/* TODO : Mobile Sidebar */}
+      <MobileSidebar />
       <div className="flex items-center gap-x-5">
         <div className="hidden md:flex">
           <Logo />
@@ -18,7 +19,11 @@ export const Navbar = () => {
         >
           Create
         </Button>
-        <Button variant="primary" className="rounded-sm bloack md:hidden">
+        <Button
+          variant="primary"
+          className="rounded-sm bloack md:hidden"
+          size="sm"
+        >
           <Plus className="h-4 w-4 shrink-0" />
         </Button>
       </div>
