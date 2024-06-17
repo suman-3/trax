f58987bdbf8a0510c2c34150ab065979d5518734
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-18 02:08:35 +0530

    hint added to main board list component

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,4 +1,5 @@
-import { User2 } from "lucide-react";
+import { Hint } from "@/components/hint";
+import { HelpCircle, User2 } from "lucide-react";
 
 export const BoardList = () => {
   return (
@@ -14,6 +15,14 @@ export const BoardList = () => {
         >
           <p className="text-sm">Create new board</p>
           <span className="text-xs">5 remaining</span>
+          <Hint
+            sideOffset={40}
+            description={`
+            Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.
+            `}
+          >
+            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+          </Hint>
         </div>
       </div>
     </div>
