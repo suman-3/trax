6af0f102fa99e8d4a984d360907e85fed7020e97
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-18 01:44:20 +0530

    Info component added with skeleton

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,25 +1,12 @@
-import { db } from "@/lib/db";
-import React, { useState } from "react";
-import { toast } from "sonner";
-import { Board } from "./board";
-import { BoardForm } from "./form";
+import { Info } from "./_components/info";
 
-const organizationPage = async ({
-  params,
-}: {
-  params: { organizationId: string };
-}) => {
-  const boards = await db.board.findMany();
+
+const organizationPage = async () => {
+ 
 
   return (
-    <div className="flex flex-col space-y-4">
-      <BoardForm />
-
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
-      </div>
+    <div className="w-full mb-20">
+     <Info/>
     </div>
   );
 };
