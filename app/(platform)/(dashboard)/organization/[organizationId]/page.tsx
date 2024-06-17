c4ecaed1c775b8e88f908b3f6c232bd070958256
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-18 02:05:21 +0530

    custom hint / tooltip  completed

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,12 +1,15 @@
+import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/info";
-
+import { BoardList } from "./_components/board-list";
 
 const organizationPage = async () => {
- 
-
   return (
     <div className="w-full mb-20">
-     <Info/>
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
