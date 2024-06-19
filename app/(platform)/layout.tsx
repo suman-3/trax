6068ea4b2e4a4c6e2dc9f87c25be04a65e98ee35
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-19 10:12:00 +0530

    query-client provider created

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,13 +2,16 @@ import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 import { ModalProvider } from "../../components/provider/modal-provider";
+import { QueryProvider } from "@/components/provider/query-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
